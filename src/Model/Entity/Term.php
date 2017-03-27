1f0f495732ae9226a6c2094340c145e2df05f9e8
Rachman Chavik <rchavik@xintesa.com>
2017-03-27 22:59:42 +0700

    Port Translate plugin

--- src/Model/Entity/Term.php
+++ src/Model/Entity/Term.php
@@ -5,7 +5,7 @@ namespace Croogo\Taxonomy\Model\Entity;
 use Cake\ORM\Behavior\Translate\TranslateTrait;
 use Cake\ORM\Entity;
 
-class Type extends Entity
+class Term extends Entity
 {
 
     use TranslateTrait;
